b092ad29b71ddb442bcbc33bd76c299b97115d18
0xgobinath <94050592+0xgobinath@users.noreply.github.com>
2022-12-28 09:45:42 +0530

    add scanner

--- main.rs
+++ main.rs
@@ -1,23 +1,42 @@
-use std::io:{Read, File, BufReader};
-use std::fs::File;
 use std::env::args;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read}
 
 pub fn main(){
     let args: Vec<String> = args().collect();
 
-    if args.len() > 1 {
+    if args.len() > 2 {
         println!({"string"})
         std::process::exit(64);
     } else if args.len() == 1 {
-        run_file(args[0]);
+        run_file(&args[1]).expect("Could not run file");
     } else {
         run_prompt();
     }
 }
 
-fn run_file(path: &String){
-    let f = File::open(path).expect("Cannot open file");
+fn run_file(path: &String) -> io::Result<()>{
+    let f = File::open(path)?;
     let mut reader = BufReader::new(f);
     let mut buf = Vec::new();
     reader.read_to_end(&mut buf);
+
+    run(&buf);
+    Ok(())
 }
+
+fn run_prompt() {
+    let stdin = io.stdin();
+    for line in stdin.lock().lines() {
+        if let Ok(line) = line{
+            if line.is_empty() {
+                break;
+            }
+            run(&line.as_bytes());
+        } else {
+            break;
+        }
+    }
+}
+
+fn run(source: &[u8]) {}
